1dc772dd34c0eb4e7ff39304a4c9c8816e1ce381
Sharon-07 <sharonhernandez07@gmail.com>
2021-04-09 09:09:13 -0600

    mod

--- pages/index.js
+++ pages/index.js
@@ -14,13 +14,13 @@ export default function Home() {
           <a href="https://hammingolay.vercel.app/">Demostraciones</a>
         </h1>
       <div className={styles.grid}>
-      <h2>Video &rarr;</h2>
-
-      </div>
-      <div className={styles.grid}>
       <h2>Presentación &rarr;</h2>
       
       <iframe src="https://onedrive.live.com/embed?cid=91F69582752FB659&amp;resid=91F69582752FB659%21115&amp;authkey=AHZiYBNqQO5ngF4&amp;em=2&amp;wdAr=1.7777777777777777" width="962px" height="565px" frameborder="0">Esto es un documento de <a target="_blank" href="https://office.com">Microsoft Office</a> incrustado con tecnología de <a target="_blank" href="https://office.com/webapps">Office</a>.</iframe>
+      </div>
+      <div className={styles.grid}>
+      <h2>Video &rarr;</h2>
+
       </div>
     
       </main>
